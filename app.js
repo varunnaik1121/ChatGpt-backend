4469f5463e6236a5713f1bf2cede5edbafa04f4f
varunnaik1121 <varunnaik1121@gmail.com>
2023-03-04 00:30:32 +0530

    updated model

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
       model: 'text-davinci-003',
       prompt: `${userInput}`,
-      temperature: 0,
+      temperature: 0.2,
       max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0.5,
